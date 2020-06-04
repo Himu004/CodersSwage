aa9d673863813f6cef68621d748abc1547645c93
Himu Chowdhury <himu.chowdhury01@gmail.com>
2020-06-04 15:09:50 +0600

    Custome Adapter Added

--- app/src/main/java/com/example/codersswag/Controller/MainActivity.kt
+++ app/src/main/java/com/example/codersswag/Controller/MainActivity.kt
@@ -1,12 +1,23 @@
 package com.example.codersswag.Controller
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import com.example.codersswag.Adapters.CategoryAdapter
+import com.example.codersswag.Model.Category
 import com.example.codersswag.R
+import com.example.codersswag.Services.DataService
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var adapter : CategoryAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        adapter = CategoryAdapter(this, DataService.categories)
+        categoryListView.adapter = adapter
     }
 }
