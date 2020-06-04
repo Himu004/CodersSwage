9039dd8eacda97db3d9f698fdb05fa67bc52fd08
Himu Chowdhury <himu.chowdhury01@gmail.com>
2020-06-04 23:35:17 +0600

    prepare for recycler view adapter

--- app/src/main/java/com/example/codersswag/Controller/MainActivity.kt
+++ app/src/main/java/com/example/codersswag/Controller/MainActivity.kt
@@ -3,7 +3,10 @@ package com.example.codersswag.Controller
 import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
+import android.widget.Toast
 import com.example.codersswag.Adapters.CategoryAdapter
 import com.example.codersswag.Model.Category
 import com.example.codersswag.R
@@ -19,5 +22,10 @@ class MainActivity : AppCompatActivity() {
 
         adapter = CategoryAdapter(this, DataService.categories)
         categoryListView.adapter = adapter
+
+        //Toast message on click the item
+        //categoryListView.setOnItemClickListener{adapterView, view: View?, position: Int, id: Long ->
+            //val category = DataService.categories[position]
+            //Toast.makeText(this, "You clicked on the ${category.title} cell", Toast.LENGTH_SHORT).show() }
     }
 }
