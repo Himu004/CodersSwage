b7eb418a47e400971b8500892cdce02b40e49431
Himu Chowdhury <himu.chowdhury01@gmail.com>
2020-06-05 00:09:30 +0600

    Checking

--- app/src/main/java/com/example/codersswag/Adapters/CategoryAdapter.kt
+++ app/src/main/java/com/example/codersswag/Adapters/CategoryAdapter.kt
@@ -11,10 +11,7 @@ import com.example.codersswag.Model.Category
 import com.example.codersswag.R
 import kotlinx.android.synthetic.main.activity_main.*
 
-class CategoryAdapter(context: Context, categories: List<Category>) : BaseAdapter( ) {
-
-    val context = context
-    val categories = categories
+class CategoryAdapter(val context: Context, val categories: List<Category>) : BaseAdapter( ) {
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
         val categoryView: View
